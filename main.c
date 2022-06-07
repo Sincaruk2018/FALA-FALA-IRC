40975f70a656110d23b3ef47fbf4606fe2e04956
Luiz Vieira <40367461+Sincaruk2018@users.noreply.github.com>
2022-06-06 21:48:37 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,16 @@
+#include <stdio.h>
 int main()
 {
+  int opt;
+  printf("Choose an option:\n1 - Server\n 2 - Client\n");
+  switch(opt)
+  {
+    case 1:
+      break;
+    case 2:
+      break;
+    default:
+      break;
+  }
   return 0;
 }
