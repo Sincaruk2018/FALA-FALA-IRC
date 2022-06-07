a09dd232e741973139c0340b6ccc87bc9eaf675b
Enrique Teles <enriquegteles@gmail.com>
2022-06-06 22:36:24 -0300

    Add methods to server.c

--- server/server.c
+++ server/server.c
@@ -9,60 +9,93 @@
 
 struct sockaddr_in address;
 
-int main() {
-    int server_fd, set_socket_res, new_socket, valread;
-    int addrlen = sizeof(struct sockaddr_in);
-    char buffer[1024] = {0};
+int createServerOnPort() {
+    int server_fd;
 
-    // create server
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {  // on error is returned -1
-        printf("Houve  algum erro ao criar servidor\n");
-        return 1;
-    }
+    return server_fd;
+}
+
+int awaitConnectionOnPort(int server, int port) {
+    int opt = 1;
+    int new_socket;
+    int addrlen = sizeof(struct sockaddr_in);
 
     // connect to a given port
-    int opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
+    if (setsockopt(server, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
                    sizeof(opt)) < 0) {
         printf("Houve algum erro ao conectar a porta\n");
-        return 1;
+        return -1;
     }
+
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    address.sin_port = htons(port);
 
     // faz o bind
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+    if (bind(server, (struct sockaddr*)&address, sizeof(address)) < 0) {
         printf("Houve algum erro ao realizar o bind\n");
-        return 1;
+        return -1;
     }
 
     // listen to a port to handle methods
-    if (listen(server_fd, 3) < 0) {
+    if (listen(server, 3) < 0) {
         printf("Houve algum erro ao escutar porta\n");
-        return 1;
+        return -1;
     }
-    // accept client
-    if (new_socket = accept(server_fd, (struct sockaddr*)&address,
+
+    if (new_socket = accept(server, (struct sockaddr*)&address,
                             (socklen_t*)&addrlen) < 0) {
         printf("Houve algum erro ao aceitar cliente\n");
         return 1;
     }
 
-    // le mensagem
-    valread = read(new_socket, buffer, 1024);
-    printf("%s\n", buffer);
+    return new_socket;
+}
 
+void sendMessage(int socket, char* msg) {
     // envia mensagem
-    char* msgSend = "Mensagem enviada pelo server";
-    send(new_socket, msgSend, sizeof(msgSend), 0);
+    send(socket, msg, sizeof(msg), 0);
+}
 
+void readMessage(int socket) {
+    // lê mensagem
+    int valread;
+    char buffer[1024] = {0};
+
+    valread = read(socket, buffer, 1024);
+    printf("%s\n", buffer);
+}
+
+void closeConnection(int socket) {
     // encerra conexão com socket
-    close(new_socket);
+    close(socket);
+}
 
+void closeServer(int server) {
     // encerra escuta de sockets
-    shutdown(server_fd, SHUT_RDWR);
+    shutdown(server, SHUT_RDWR);
+}
+
+int main() {
+    int server = createServerOnPort();
+    if (server < 0) {
+        printf("Houve  algum erro ao criar servidor\n");
+        return 1;
+    }
+
+    int conn_socket = awaitConnectionOnPort(server, 8080);
+    if (conn_socket < 0) {
+        printf("Não foi possível se conectar a um cliente\n");
+    }
+
+    readMessage(server);
+
+    char* msg = "Mensagem enviada pelo servidor";
+    sendMessage(conn_socket, msg);
+
+    closeConnection(conn_socket);
+    closeServer(server);
 
     return 0;
 }
